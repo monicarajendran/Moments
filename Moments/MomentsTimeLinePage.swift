2325f0ff8f9cdad3fb3540f5d7d55835d274ef6c
Monica <monica@full238-prt.full.creative>
2018-04-10 15:36:29 +0530

    Add moment button redesigned Changes in Storyboard and moments timeline section header Added date order to moment cards

--- Moments/MomentsTimeLinePage.swift
+++ Moments/MomentsTimeLinePage.swift
@@ -16,17 +16,13 @@ import Firebase
 class MomentsTimeLinePage: UIViewController , UITableViewDataSource,UITableViewDelegate,UIScrollViewDelegate,UISearchBarDelegate{
     
     @IBOutlet weak var timelineSearchBar: UISearchBar!
-    
     @IBOutlet weak var tableView: UITableView!
-    
     @IBOutlet weak var noResultsFound: UILabel!
+    @IBOutlet weak var addButtonOutlet: UIButton!
     
     var searchBarActive = false
-    
     var searchBarText = ""
-    
     var momentObject : Moment?
-    
     var filteredObjects = Table<Moment>(context: container.viewContext)
     
     lazy var fetchTheMoments : FetchRequestController<Moment> = {
@@ -35,7 +31,6 @@ class MomentsTimeLinePage: UIViewController , UITableViewDataSource,UITableViewD
         let sortByDate = NSSortDescriptor(key: "momentDate", ascending: false)
         
         let query = container.viewContext.moment.sort(using: [sortByDate, sortByTime])
-        
         return query.toFetchRequestController(sectionNameKeyPath: "momentDate", cacheName: nil)
         
     }()
@@ -43,23 +38,20 @@ class MomentsTimeLinePage: UIViewController , UITableViewDataSource,UITableViewD
     let dateFormatter = DateFormatter()
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         
         UserDefaults.standard.set(true, forKey: "firstRun")
-        
         UserDefaults.standard.synchronize()
         
+        addButtonShadow()
         dateFormatter.dateStyle = .long
         
         tableView.delegate = self
-        
         timelineSearchBar.delegate = self
         
         self.queryTheDataFromDisk()
         
         tableView.tableFooterView = UIView(frame: .zero)
-        
         navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
         
         //to remove the bottom and top line of the search bar
@@ -73,7 +65,6 @@ class MomentsTimeLinePage: UIViewController , UITableViewDataSource,UITableViewD
         self.navigationController?.navigationBar.isHidden = false
         self.navigationController?.navigationBar.topItem?.title = "Moments"
         noResultsFound.isHidden = true
-        
         self.tableView.reloadData()
     }
     
@@ -90,30 +81,22 @@ class MomentsTimeLinePage: UIViewController , UITableViewDataSource,UITableViewD
         searchBar.setShowsCancelButton(true, animated: true)
         return true
     }
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        
-        print("entered search bar")
         
         Analytics.logEvent("moment_search", parameters: ["search_word": searchBar.text ?? ""])
         
         let searchPredicate = NSPredicate(format: "searchToken CONTAINS[c] %@",searchText)
-        
         filteredObjects = container.viewContext.moment.filter(using: searchPredicate).sort(using: NSSortDescriptor(key: "createdAt", ascending: false))
-        
-        print(searchText)
         
         searchBarText = searchText
         
-        print("fetched objects: \(filteredObjects.count())")
-        
         if filteredObjects.count() == 0 {
             searchBarActive = false
-        }
-        else{
+        } else{
             self.tableView.backgroundView = .none
             searchBarActive = true
         }
-        
         self.tableView.reloadData()
     }
     
@@ -121,9 +104,7 @@ class MomentsTimeLinePage: UIViewController , UITableViewDataSource,UITableViewD
         
         do {
             try fetchTheMoments.performFetch()
-        }
-        catch{
-            
+        } catch{
             print("error occured")
         }
     }
@@ -131,22 +112,29 @@ class MomentsTimeLinePage: UIViewController , UITableViewDataSource,UITableViewD
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         
         searchBar.resignFirstResponder()
-        
         searchBarText = ""
         searchBar.text = nil
-        
         searchBarActive = false
         searchBar.setShowsCancelButton(false, animated: true)
         tableView.reloadData()
     }
     
+    func addButtonShadow() {
+        
+        let buttonLayer = addButtonOutlet.layer
+        buttonLayer.cornerRadius = addButtonOutlet.frame.height / 2
+        buttonLayer.shadowColor = UIColor.black.withAlphaComponent(0.5).cgColor
+        buttonLayer.shadowOffset = CGSize(width: 0, height: 1)
+        buttonLayer.shadowRadius = 5
+        buttonLayer.shadowOpacity = 0.7
+        buttonLayer.masksToBounds = false
+    }
     
     @IBAction func addNewMoment(_ sender: UIButton) {
         
         guard let createMomentsVC = R.storyboard.main.createMomentsViewController() else {
             return
         }
-        
         let navController = UINavigationController(rootViewController: createMomentsVC)
         self.present(navController, animated: true, completion: nil)
     }
@@ -167,23 +155,26 @@ class MomentsTimeLinePage: UIViewController , UITableViewDataSource,UITableViewD
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 40))
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 50))
         view.backgroundColor = .clear
-        
+
         let dateFormater = DateFormatter()
         dateFormater.dateFormat = MomentDateFormat.monthAndYear.rawValue
         
         let label = UILabel()
         label.textColor = UIColor.black.withAlphaComponent(0.6)
         label.text = dateFormater.string(from: momentObject?.momentDate ?? Date())
-        label.frame = CGRect(x: 20, y: 5, width: tableView.frame.width, height: 30)
-        view.addSubview(label)
+        label.frame = CGRect(x: 20, y: 15, width: tableView.frame.width, height: 30)
         
+        //Section headers will now scroll 
+        self.tableView.contentInset = UIEdgeInsetsMake(-40, 0, 0, 0)
+        view.addSubview(label)
+
         return view
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 40
+        return 50
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -195,53 +186,42 @@ class MomentsTimeLinePage: UIViewController , UITableViewDataSource,UITableViewD
         }
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! MomentsTableViewCell
         
         if searchBarActive {
-            
             momentObject = filteredObjects.execute()[indexPath.row]
-        }
-        else {
-            
+        } else {
             momentObject = fetchTheMoments.object(at: indexPath)
         }
         
         if (filteredObjects.count() == 0 && !searchBarText.isEmpty) {
-            
             cell.isHidden = true
             noResultsFound.isHidden = false
-        }
-            
-        else {
-            
+        } else {
             noResultsFound.isHidden = true
         }
         
         cell.momentName.text = momentObject?.name
-        
         cell.momentDescription.text = momentObject?.desc
-        
         cell.date.text = "\(String(format: "%02d", momentObject?.day ?? 0))"
         
-        if let color = momentObject?.color
-        {
-            
+        if let color = momentObject?.color {
             cell.viewForCell.backgroundColor = UIColor(hexString: color)
             cell.viewForDate.backgroundColor = UIColor(hexString: color)
         }
         
         let timeAsSeconds = momentObject?.momentTime
-        
         let date = Date(timeIntervalSince1970: TimeInterval(timeAsSeconds ?? 0))
         
         dateFormatter.dateFormat = MomentDateFormat.day.rawValue
-        
         cell.day.text = dateFormatter.string(from: date).uppercased()
-        
         cell.viewForCell.layer.cornerRadius = 5
-        
         dateFormatter.dateFormat = MomentDateFormat.monthAndYear.rawValue
         
         return cell
@@ -256,8 +236,7 @@ class MomentsTimeLinePage: UIViewController , UITableViewDataSource,UITableViewD
         }
         if searchBarActive{
             momentObject = filteredObjects.execute()[indexPath.row]
-        }
-        else{
+        } else{
             momentObject = fetchTheMoments.object(at: indexPath)
         }
         
